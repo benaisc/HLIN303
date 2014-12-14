da3450fe7d7895d8230c2212be92486cb2bd1b84
Charles <gurujam@users.noreply.github.com>
2014-12-14 18:16:36 +0100

    Update fonction.h

--- fonction/fonction.h
+++ fonction/fonction.h
@@ -2,7 +2,7 @@
 #include <stdio.h>
 void plus_une_case(char * a);
 int deuxp(int a);
-unsigned int* convert_d_b(int a);
+unsigned char* convert_d_b(int a);
 int convert_b_d( unsigned int * p);
 int taille_c(char * p);
 void init( unsigned int *tab);
