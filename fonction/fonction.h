2ac15a8bdbdc62003b8b2cbc306bbb4fd2bafec2
babash <victor.connes@gmail.com>
2014-12-15 12:53:33 +0100

    Update fonction.h

--- fonction/fonction.h
+++ fonction/fonction.h
@@ -2,7 +2,7 @@
 #include <stdio.h>
 void plus_une_case(char* a);
 int deuxp(int a);
-char* convert_d_b(int a);
+char* convert_d_b(char a);
 int convert_b_d(char* p);
 int taille_c(char* p);
 void init(char *tab);
