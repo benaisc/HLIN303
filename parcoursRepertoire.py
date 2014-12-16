2cdb82d0520134e1e5608e29f0d07d1beca5ac67
Charles <gurujam@users.noreply.github.com>
2014-12-16 23:47:12 +0100

    Update parcoursRepertoire.py

--- parcoursRepertoire.py
+++ parcoursRepertoire.py
@@ -1,22 +1,16 @@
-#!/usr/bin/env python
+#!/usr/local/bin/python
+import sys, os
 
-import os, sys
-import os
-
-Fichiers={"":0}
-
-def parcours(rep):
-    print "je suis dans : ",rep
-    liste=os.listdir(rep)
-    for fichier in liste:
-        if os.path.isdir(rep+"/"+fichier):
-            parcours(rep+'/'+fichier)
-        else:
-            os.system('huf'+' '+'fichier')
-
-parcours(sys.argv[1])
-total=0
-for fic in Fichiers:
-        print fic, ":", Fichiers[fic]
-        total+=Fichiers[fic]
-print "total :", total
+if len(sys.argv)==2:
+	def parcours(rep):
+		print "je suis dans : ",rep
+		liste=os.listdir(rep)
+		for fichier in liste:
+			if os.path.isdir(rep+"/"+fichier):
+				parcours(rep+'/'+fichier)
+			else:
+				print "je compresse : ",fichier
+				os.system('./huf'+' '+fichier+' '+fichier+'huf')
+	parcours(sys.argv[1])
+else:
+	print "Syntax: $parcoursRepertoire.py /REP"
