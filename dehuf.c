3b054333ac3a422e6cb378ccb3ecb2b3f4cf9481
Charles <gurujam@users.noreply.github.com>
2014-12-19 15:51:15 +0100

    dehuf remplit la struct des symboles et leurs codes

--- dehuf.c
+++ dehuf.c
@@ -1,7 +1,5 @@
 #include "./arbre/arbre.h"
 #include "./fonction/fonction.h"
-#include <stdio.h>
-#include <string.h>
 
 int main(int argc, char* argv[]){
 
@@ -10,76 +8,114 @@ if (argc!=3){
 printf("Syntax Error.\n");
 return 1;
 }
-FILE *f=fopen(argv[1],"r");
-if(!f){
+FILE *fic=fopen(argv[1],"r");
+if(!fic){
 printf("Fichier non trouvé.\n");
-fclose(f);
+fclose(fic);
 return 2;
 }
-int c=fgetc(f);
+int c=fgetc(fic);
 if(c==EOF){
 printf("Fichier vide.\n");
-fclose(f);
+fclose(fic);
 return 3;
 }
+fclose(fic);
 
+//Decodage de l'entete
+	FILE *x=fopen(argv[1],"r");//argv[1] = fichierhuf.txt
+	unsigned char car;
+	car=fgetc(x);
+//printf("1er char lu = %d\n",car);
+	unsigned int nbBitsLire=((int) car);
+//printf("Bits à lire : %d\n",nbBitsLire);//nombre de bits a lire dans les char qui suivent pour connaitre le nombre de bits à lire une fois la table d'encodage crée
+ 	unsigned char* CODE_BIN;
+	car=fgetc(x);
+//printf("2e char lu : %d\n",car);
+ 	CODE_BIN=int_to_bin((int)car);
+//printf("Son code binaire : %s\n",CODE_BIN);
+	unsigned char* CODE_BIN_SUP=malloc(nbBitsLire*sizeof(char)+1);
 
-// ./dehuf fichierhuf.txt
-/*Remplissage d'un tab avec l'en-tete*/
- FILE *x=fopen(argv[1],"r");//argv[1] = fichierhuf.txt
- char car=43;
- int nbnoeud=-1,indice=0;
- int i=-1;
- int temp1=-128,temp2=-129;
- NoeudSC *Arbre=malloc(sizeof(NoeudSC));
- nbnoeud=0;indice=0;
- while(!((temp1=='&')&& (temp2=='&'))){
-   car=fgetc(x);
-   temp2=temp1;
-   temp1=(int)car; //entier du code ascii correspondant ex: nbnoeud:74 
-   if(car=='&'){
-   }
-   else{
-     if(temp2=='&'){
-       nbnoeud+=1;
-       Arbre=realloc(Arbre,nbnoeud*sizeof(NoeudSC));
-       Arbre[nbnoeud-1].symbole=car;
-       i-=1;
-       indice=0;
-     }
-     else if (i==-1){
-       nbnoeud+=1;
-       Arbre=realloc(Arbre,nbnoeud*sizeof(NoeudSC));
-       if(Arbre==NULL) {
-	 printf("JE SUIS ICI\n");
-       }
-       Arbre[nbnoeud-1].symbole=car;
-       i-=1;
-       indice=0;
-     }
-     else if (i==-2){
-       i=atoi(&car)-1;
-       printf("car=%d\n",i+1);
-       Arbre[nbnoeud-1].code=malloc((i+1)*sizeof(char));
-       Arbre[nbnoeud-1].code[i]='\0';
+	if(nbBitsLire<=8){
+	    int i=0;
+		for(i=0;i<nbBitsLire;i++){
+			CODE_BIN_SUP[i]=CODE_BIN[i];
+		}
+	CODE_BIN_SUP[i+1]='\0';
+ //printf("CODE_BIN_SUP=%s\n",CODE_BIN_SUP);
+	car=fgetc(x);//car=&
+	}
+	else{
+		int i=0;
+		for(i=0;i<8;i++){
+			CODE_BIN_SUP[i]=CODE_BIN[i];
+		}
+		car=fgetc(x);
+		CODE_BIN=int_to_bin((int)car);
+		int j=8;
+		while(car != '&'){
+		i=0;
+			while((j < nbBitsLire)&&(i < 8)){
+				CODE_BIN_SUP[j]=CODE_BIN[i];
+				j+=1;
+				i+=1;
+			}
+		CODE_BIN_SUP[nbBitsLire]='\0';
+		car=fgetc(x);//car=&
+		}
+//printf("CODE_BIN_SUP=%s\n",CODE_BIN_SUP);
+	free(CODE_BIN);
+	}
+	unsigned int nbBitsTotal=bin_to_dec(CODE_BIN_SUP);
+	printf("nbBits d'encodage=%d\n",nbBitsTotal);
+	free(CODE_BIN_SUP);
 
-       if(Arbre[nbnoeud-1].code==NULL) {
-	 printf("JE SUIS LA\n");
-       }
-     }
-     else{
-       Arbre[nbnoeud-1].code[indice]=car;
-       i-=1;
-       indice+=1;
-     }  
-   }
- }
- int GRR=0;
- for(GRR=0;GRR<nbnoeud;GRR++){
-   int taille=0;
-taille= taille_c(Arbre[GRR].code);
-  printf("Arbre[%d].symbole=%c Arbre[%d].code=%s de taille=%d\n",GRR,Arbre[GRR].symbole,GRR,Arbre[GRR].code,taille);
- }
+//Remplissage d'une structure de décodage avec
+	NoeudSC *Arbre=NULL;
+	int nbnoeud=0;
+	int ind=0;
+	int i=-1;
+	int k=0;
+	char CHECK=NULL;
+	car=fgetc(x);
+//printf("Car de départ = %c\n",car);
+	while((car != '&')&&((CHECK=fgetc(x)) != '&')){
+//printf("STAGE %d : car = %c - CHECK=%c avec i=%d\n",nbnoeud,car,CHECK,i);
+		if(i==-1){
+			nbnoeud+=1;
+			Arbre=realloc(Arbre,nbnoeud*sizeof(NoeudSC));
+			Arbre[nbnoeud-1].symbole=car;
+			i-=1;
+//printf("i==-1 -> Arbre[%d].symbole=%c\n",nbnoeud-1,Arbre[nbnoeud-1].symbole);
+			car=CHECK;
+		}
+		else{
+			ind=((int) car)-48;
+//printf("Taille du code : ind=%d\n",ind);
+			Arbre[nbnoeud-1].code=malloc((ind+1)*sizeof(char));
+			for(k=0;k<ind;k++){
+				Arbre[nbnoeud-1].code[k]=CHECK;
+				CHECK=fgetc(x);
+			}
+		Arbre[nbnoeud-1].code[ind]='\0';
+		car=CHECK;
+		i=-1;
+//printf("i==-2 -> Arbre[%d].code=%s\n",nbnoeud-1,Arbre[nbnoeud-1].code);
+		}
+	}
 
+/*   
+	while(CPT<nbBitsTotal){
+		mettre bit à bit dans un char* et tester tous les codes, CPT++
+		si un code correspond coller le symbole
+	}
+*/
+
+	int G=0;
+	for(G=0;G<nbnoeud;G++){
+		printf("Arbre[%d] : symbole=%c code=%s\n",G,Arbre[G].symbole,Arbre[G].code);
+	}
+ 
+free(Arbre);
 return 0;
 }
